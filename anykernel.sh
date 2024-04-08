8c806813600492aec9186b091959f310a5935d39
Light_summer <93428659+lightsummer233@users.noreply.github.com>
2024-04-08 21:04:23 +0800

    Change kernel author's name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=アリエス Kernel by Dimuuni
+kernel.string=アリエス Kernel by rcmiku
 do.devicecheck=1
 do.modules=0
 do.systemless=1
